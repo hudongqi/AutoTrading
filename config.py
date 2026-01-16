9661be4c250cd49412906a7785dfb312cc6a6104
dongqi hu <hudongqi4869@gmail.com>
2026-01-16 15:37:03 +0900

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+SYMBOL = "BTC/USDT:USDT"      # Binance USDT 永续
+START = "2026-01-01"
+END = "2026-02-01"
+
+INITIAL_CASH = 10_000
+
+FEE_RATE = 0.0004           # 0.04% taker
+SLIPPAGE_BPS = 5            # 0.05%
