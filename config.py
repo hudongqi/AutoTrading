4c464c7939914b922dd4f8ecbd3b11aea8fee007
dongqi hu <hudongqi4869@gmail.com>
2026-03-08 14:57:03 +0900

    调试

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 SYMBOL = "BTC/USDT:USDT"      # Binance USDT 永续
 START = "2025-11-01"
-END = "2026-01-24"
+END = "2026-03-01"
 
-INITIAL_CASH = 100000
+INITIAL_CASH = 10000
 
 FEE_RATE = 0.0004           # 0.04% taker
 SLIPPAGE_BPS = 5            # 0.05%
